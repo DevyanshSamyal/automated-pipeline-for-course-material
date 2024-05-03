a38e810b40029a709a1191a16f80bb9335d9863e
DevyanshSamyal <89250437+DevyanshSamyal@users.noreply.github.com>
2024-05-03 16:29:01 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "system-monitoring-bucket" # Replace with your actual S3 bucket name
+    bucket = "file-upload-app-bucket" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
